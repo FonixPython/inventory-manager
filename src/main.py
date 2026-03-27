220ca9e64d1363c9bdfe68458c1590b2bff70ddc
skyFonix <vlevente.main1123@gmail.com>
2026-03-27 21:13:46 +0100

    starting commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
