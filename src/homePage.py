6aad1d9bd6950031f3a8accaf6aa637bff8e8c6c
skyFonix <vlevente.main1123@gmail.com>
2026-03-29 13:43:55 +0200

    requirements + slight restructure

---
+++ src/homePage.py
@@ -0,0 +1,7 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow,QLineEdit, QWidget,QLabel, QPushButton,QHBoxLayout,QVBoxLayout,QSizePolicy
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIntValidator
+
+from db import check_mysql_connection
+
+
